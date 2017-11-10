a61fdb541805009da725a9fd987cae3ea0f0fa5a
Lamuchy <ichibunny@hotmail.com>
2017-11-10 14:02:30 +0000

    Update sw-note.js

--- sw-note.js
+++ sw-note.js
@@ -18,6 +18,69 @@ IndexController.prototype._registerServiceWorker = function() {
   });
 };
 // registration of sw needs to be done on indexController where all contructer lives
+/*
+when you register for sw, it returns promise, that promise fulfills with a sw object (which has property and mehod)
+
+*/
+
+ navigator.serviceWorker.register('/sw.js').then(function(reg) {
+   reg.unregister();
+   reg.update();
+   
+   reg.installing;
+   reg.waiting;
+   reg.active;
+   // these will be point at service worker object or null
+   reg.addEventListner('updatefound', function (){
+     //reg.installing has changed - becomw a new worker
+   })
+   
+ });
+
+var sw = reg.installing;
+console.log(sw.state); //... log "installing"
+/* state can be 
+"installed"
+" activating"
+"activated"
+"redundant"
+*/
+
+sw.addEventlistner ('statechange', function(){
+  
+  //sw.state has changed
+})
+
+ navigator.serviceWorker.controller
+// referes to the sw that control this page
+// you wanna tell user when the update ready,
+
+if (! navigator.serviceWorker.controller)
+  // it means this page did not load using service worker loaded from network
+  
+if (reg.waiting)
+   // means update ready and waiting
+  indexController._updateReady();
+if (reg.installing)
+  // means update in progress
+   reg.installing.addEventListner ('statechange', function (){
+     if (this.state == 'installed){
+         //update ready
+         }
+   })
+    // other wise listen for the update found event
+    // that fires we tack the satate ofinstalling worker and if it reached installed state we wil tell user.
+    reg.addEventListener ('updatefound', function (){
+      reg.installing.addEventlistener('statechange', function (){
+        if (this.state == 'installed'){
+          // update ready
+        }
+      })
+    });
+                          
+  
+
+
 
 
 self.addEentlistener ('fetch', function (event) {       // fetcheventlistner
@@ -60,5 +123,27 @@ self.addEventListener('fetch', function(event) {
     }
     return fetch(event.request);
   })
+  );
+});
+
+
+
+
+// more practical way, use var for chache name
+//in practice, use version nacme on cache name as well as file name - i.e. css/main-8gd9.css
+var staticCacheName = 'wittr-static-v2';
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then (function (cacheNames){
+      return Promise.all (
+        cacheNames.filter().filter(function(cacheName){
+        return cacheName.statusWith('wittr-')&&
+          cacheName != staticCacheName;
+        }).map(function (cacheName){
+          return caches.delete(cacheName);
+        })
+      )  
+    })
   );
 });
