8498e801c7658b61bb062a7b94e9ba3e01ed6e18
Lamuchy <ichibunny@hotmail.com>
2017-11-19 12:00:52 +0000

    Update idb_promised.js

--- idb_promised.js
+++ idb_promised.js
@@ -230,3 +230,74 @@ UpgradeDB:  As DB, except---------------------------------------------------
                   tx.objectStore('keyval').put('hello', 'world');
                   return tx.complete;
                 }).then(() => console.log("Done!"));
+      
+ // code for  cache img 
+ 
+ var staticCacheName = 'wittr-static-v5';
+var contentImgCache = 'wittr-content-imgs';
+// store all cache in arr
+var allcaches = [
+  staticCacheName,
+  contentImgCache
+];
+//
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/skeleton',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+                !allcaches.include (cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);        // UEL constructor takes url
+
+  if (requestUrl.origin === location.origin) {        // URL constructor property origin is the url
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+     if (requestUrl.pathname.startWith ('/photo/')){
+       event.respondWith (servePhoto (event.request));  // return the respond
+       return;
+     }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+function servePhoto(request){
+  var storageUrl = request.url.replace(/-\d+px\.jpeg$/, '') // replace the jpeg
+} 
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
